fd1c3bde178b9b18e354d62ec3a11f21aa3d13c1
lcylcy1 <1277592267@qq.com>
2020-04-15 20:32:45 +0800

    。。

--- php/logon_check.php
+++ php/logon_check.php
@@ -8,7 +8,9 @@ if($row=mysqli_fetch_array($result)){
     
   
     if($row["password"]==$password){
-
+        
+        setCookie("username",$username);//..设置一个用户名COOKIE
+        setCookie("isLogin",1);//..设置一个登录判断的标记isLogin
         echo 1;
 
     }else{
