e7c7f369bf4f239cb5b56b7abc3a03b6d3fe6013
lcylcy1 <1277592267@qq.com>
2020-04-24 14:58:06 +0800

    111

--- php/conn.php
+++ php/conn.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
  // 创建连接
-$conn = mysqli_connect("localhost", "root", "123456","administration") or die("连接失败");
+$conn = mysqli_connect("localhost", "root", "root","administration") or die("连接失败");
 mysqli_query($conn,"set names utf8");
 // 检测连接
 
